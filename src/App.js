e523e7957e873b872f946033f67743ebc3b15c66
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-06-16 19:06:21 +0200

    Add state and props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-//Import Styles
+import React, { useState } from "react";
+
+// Import Styles
 import "./styles/app.scss";
 
 // Adding Components
-import Player from './components/Player';
-import Song from './components/Song';
+import Player from "./components/Player";
+import Song from "./components/Song";
+
+// Import data
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <Song />
-      <Player />
-    </div>
-  );
+	// State
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+	return (
+		<div className="App">
+			<Song currentSong={currentSong} />
+			<Player
+				setIsPlaying={setIsPlaying}
+				isPlaying={isPlaying}
+				currentSong={currentSong}
+			/>
+		</div>
+	);
 }
 
 export default App;
