e37324e05130c210c3fb83b12d649c65c0ed714b
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-06-16 20:03:36 +0200

    Add dragHandler and fix null error

--- src/components/Player.js
+++ src/components/Player.js
@@ -12,6 +12,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 	const audioRef = useRef(null);
 
 	// Event Handlers
+
 	const playSongHandler = () => {
 		if (isPlaying) {
 			audioRef.current.pause();
@@ -31,16 +32,28 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 			Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
 		);
 	};
+	const dragHandler = (e) => {
+		audioRef.current.currentTime = e.target.value;
+		setSongInfo({ ...songInfo, currentTime: e.target.value });
+	};
+
 	// State
+
 	const [songInfo, setSongInfo] = useState({
-		currentTime: null,
-		duration: null,
+		currentTime: 0,
+		duration: 0,
 	});
 	return (
 		<div className="player">
 			<div className="time-control">
 				<p>{getTime(songInfo.currentTime)}</p>
-				<input type="range" />
+				<input
+					min={0}
+					max={songInfo.duration}
+					value={songInfo.currentTime}
+					onChange={dragHandler}
+					type="range"
+				/>
 				<p>{getTime(songInfo.duration)}</p>
 			</div>
 			<div className="play-control">
@@ -59,6 +72,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 			</div>
 			<audio
 				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
 				ref={audioRef}
 				src={currentSong.audio}
 			></audio>
